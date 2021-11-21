d8ea9c779a3963988bd38bdeaa1fbbaf40bafcdc
Ginn <56385383+ginn723@users.noreply.github.com>
2021-11-22 00:43:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+  res.render('homepage', {
+    title: '首頁' //傳入頁面title
+  })
+})
 
 app.get('/:page', (req, res) => {
   const page = req.params.page
